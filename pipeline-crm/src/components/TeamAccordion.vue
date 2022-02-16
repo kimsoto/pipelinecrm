206c293757adda4c013afc5dfc03f8867964a00a
Kim S <kimx801@gmail.com>
2022-02-16 16:14:15 -0700

    attempting to connect API to front end

--- pipeline-crm/src/components/TeamAccordion.vue
+++ pipeline-crm/src/components/TeamAccordion.vue
@@ -1,9 +1,9 @@
-<template v-for="team in teams">
-    <div :key="team.name" class="col-xs-12 col-md-4">
+<template :key="team.name" v-for="team in teams">
+    <div class="col-xs-12 col-md-4">
         <div class="accordion-grid">
         <figure @click="toggle" class="accordion autoclose" aria-controls="accordion0content" role="button">
         <figcaption>
-            <p>{{ team.name }}</p>
+            <p>{{ team[0].name }}</p>
         </figcaption>
         </figure>
         <div class="accordion-content padding-15 padding-top-0 border-radius-10 margin-top-15" role="region" v-show="hideGrid">
