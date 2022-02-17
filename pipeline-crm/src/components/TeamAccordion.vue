7565a625cbd2cdac6e7e55fd129bf56ff8605c75
Kim S <kimx801@gmail.com>
2022-02-16 17:04:34 -0700

    attempting to connect API to front end

--- pipeline-crm/src/components/TeamAccordion.vue
+++ pipeline-crm/src/components/TeamAccordion.vue
@@ -3,7 +3,7 @@
         <div class="accordion-grid">
         <figure @click="toggle" class="accordion autoclose" aria-controls="accordion0content" role="button">
         <figcaption>
-            <p>{{ team.name }}</p>
+            <p>{{ team }}</p>
         </figcaption>
         </figure>
         <div class="accordion-content padding-15 padding-top-0 border-radius-10 margin-top-15" role="region" v-show="hideGrid">
@@ -50,7 +50,7 @@ export default {
     methods: {
         toggle() {
             this.hideGrid = !this.hideGrid
-            console.log(this.props)
+            console.log(this.teams)
         }
     }
 }
