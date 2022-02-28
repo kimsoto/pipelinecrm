329086c256678b93d02cea3772d56e309008f987
Kim S <kimx801@gmail.com>
2022-02-28 16:39:19 -0700

    adding pipeline api to Teams page

--- pipeline-crm/src/components/TeamAccordion.vue
+++ pipeline-crm/src/components/TeamAccordion.vue
@@ -55,9 +55,10 @@ export default {
     },
     mounted() {
       Promise.all(axios.get(`/api/member/${this.team.team_id}/All`), axios.get(`/api/pipeline/${this.team.team_id}/All`))
-        .then(results => {
-          this.members = results[0].data
-          this.pipelines = results[1].data
+      .then(results => {
+          // this.members = results[0].data
+          // this.pipelines = results[1].data
+          console.log(results)
       })
   }
 }
